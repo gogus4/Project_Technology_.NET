2ab2885764508c9d2432bfe7e0dd9162d9b14cbe
gogus4 <diego.dacosta@hotmail.fr>
2014-11-14 19:34:11 +0100

    Linq To Entities

---
+++ Carmotub/Views/UpdateIntervention.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Carmotub.Model;
+using Carmotub.ViewModel;
+using Carmotub.Views.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Carmotub.Views
+{
+    public partial class UpdateIntervention : Window
+    {
+        public Intervention Intervention { get; set; }
+
+        public UpdateIntervention(Intervention intervention)
+        {
+            InitializeComponent();
+
+            Intervention = intervention;
+            this.DataContext = this;
+        }
+
+        private async void UpdateIntervention_Click(object sender, RoutedEventArgs e)
+        {
+            /*double montant;
+            bool result = Double.TryParse(Montant.Text.Replace(".", ","), out montant);
+
+            Intervention intervention = new Intervention()
+            {
+                identifiant = Intervention.identifiant,
+                carnet = Carnet.Text,
+                date_intervention = Convert.ToDateTime(DateIntervention.Text),
+                identifiant_client = Intervention.identifiant_client,
+                montant = montant,
+                nature = Nature.Text,
+                numero_cheque = NumeroCheque.Text,
+                type_chaudiere = TypeChaudiere.Text,
+                type_paiement = TypePaiement.Text
+
+            };
+
+            if (await InterventionVM.Instance.UpdateIntervention(intervention) == true)
+            {
+                UpdateCustomer.Instance.RefreshDataGridInterventions();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Une erreur est intervenue lors de la modification de l'intervention.");
+            }*/
+        }
+
+        private async void DeleteInterventionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (await InterventionVM.Instance.DeleteIntervention(Intervention) == true)
+            {
+                //if (await InterventionVM.Instance.GetAllIntervention() == true)
+                //{
+                UpdateCustomer.Instance.RefreshDataGridInterventions();
+                this.Close();
+                //}
+
+                //else MessageBox.Show("Une erreur est intervenue lors de la suppression du client.");
+            }
+
+            else MessageBox.Show("Une erreur est intervenue lors de la suppression du client.");
+        }
+    }
+}
---
+++ Carmotub/Views/UpdateUser.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Carmotub.Model;
+using Carmotub.ViewModel;
+using Carmotub.Views.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Carmotub.Views
+{
+    public partial class UpdateUser : Window
+    {
+        public Customer CustomerToUpdate { get; set; }
+
+        public UpdateUser(Customer customer)
+        {
+            InitializeComponent();
+
+            CustomerToUpdate = customer;
+            this.DataContext = this;
+
+            var list_interventions = InterventionVM.Instance.Interventions.Where(x => x.identifiant_client == CustomerToUpdate.identifiant);
+            DataGridInterventions.ItemsSource = list_interventions;
+
+            Commentaire.Document.Blocks.Clear();
+            Commentaire.Document.Blocks.Add(new Paragraph(new Run(CustomerToUpdate.commentaire)));
+        }
+
+        private async void UpdateCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            string richText = new TextRange(Commentaire.Document.ContentStart, Commentaire.Document.ContentEnd).Text;
+            Customer customer = new Customer()
+            {
+                identifiant = CustomerToUpdate.identifiant,
+                adresse = Adresse.Text,
+                code = Code.Text,
+                code_postal = CodePostal.Text,
+                commentaire = richText,
+                nom = Name.Text,
+                prenom = Firstname.Text,
+                ville = Ville.Text,
+                etage = Etage.Text,
+                escalier = Escalier.Text,
+                telephone_1 = TelFixe.Text,
+                telephone_2 = TelPortable.Text,
+                recommande_par = RecommandePar.Text,
+                Rdv = Rdv.Text
+            };
+
+            if (await CustomerVM.Instance.UpdateCustomer(customer) == true)
+            {
+                await ActionsCustomers.Instance.Init();
+
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Une erreur est intervenue lors de la modification du client.");
+            }
+        }
+    }
+}
